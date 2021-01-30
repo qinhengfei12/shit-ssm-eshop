de927826396598f54fd516c4aeea936e52534650
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2021-01-31 01:58:10 +0800

    init 1

---
+++ src/main/java/xyz/kmahyyg/eshopdemo/main/NsfocusShopApplication.java
@@ -0,0 +1,13 @@
+package xyz.kmahyyg.eshopdemo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsfocusShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NsfocusShopApplication.class, args);
+    }
+
+}
