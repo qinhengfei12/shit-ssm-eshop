63d5d6d0abff775ce405e162078d751dc6477c79
Papillon-debug <ssh1146576451@163.com>
2021-02-09 20:13:15 +0800

    add cart template

--- src/main/java/xyz/kmahyyg/eshopdemo/controller/ShopContoller.java
+++ src/main/java/xyz/kmahyyg/eshopdemo/controller/ShopContoller.java
@@ -1,4 +1,14 @@
 package xyz.kmahyyg.eshopdemo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ShopContoller {
+    @RequestMapping("/show/user/cart")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public String showCart(){
+        return "cart";
+    }
 }
