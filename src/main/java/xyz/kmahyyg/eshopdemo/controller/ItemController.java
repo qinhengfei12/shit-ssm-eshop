514873dc261555d08d69132407a520243f265534
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2021-02-10 22:57:28 +0800

    CJK chars passed to redirected route incorrectly
    fix #2

--- src/main/java/xyz/kmahyyg/eshopdemo/controller/ItemController.java
+++ src/main/java/xyz/kmahyyg/eshopdemo/controller/ItemController.java
@@ -4,33 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import xyz.kmahyyg.eshopdemo.dao.SysItemsDao;
 import xyz.kmahyyg.eshopdemo.model.SysItems;
 import java.util.List;
 import xyz.kmahyyg.eshopdemo.utils.UserInputSanitizer;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ItemController {
     @Autowired
     private SysItemsDao sysItemsDao;
 
-    @RequestMapping("show/item/detail")
+    @RequestMapping("/show/item/detail")
     public String getItemDetail(Model model,Integer itemId){
         SysItems showItem = sysItemsDao.selectById(itemId);
         if(showItem != null){
+            // USE WITH CAUTION
+            // the user input of item detail from any shop owner should be untrusted.
+            // Since this project does NOT include any management panel, so here I'm so lazy to sanitize about it.
+            // Preventing from XSS is on the way.
             model.addAttribute("showItem", showItem);
             return "itemDetail";
         }else{
             return "error";
         }
     }
-    @RequestMapping("show/item/search")
-    public String searchResult(Model model, String itemName){
+    @RequestMapping("/show/item/search")
+    public String searchResult(Model model, @RequestParam("itemName") String itemName){
         List<SysItems> searchItems = sysItemsDao.selectByItemName(itemName);
         if(searchItems == null){
             return "error";
         }
-        model.addAttribute("searchKey", itemName);
+        model.addAttribute("searchKey", UserInputSanitizer.SanitizeForHTML(itemName));
         model.addAttribute("searchResult", searchItems);
         return "search";
     }
