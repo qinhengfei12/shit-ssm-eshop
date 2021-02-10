0d11f7dc3bed5537f89dcb67b350f668722c6f25
fortheone <yh1013024906@outlook.com>
2021-02-10 19:54:30 +0800

    添加了订单页面的前端

--- src/main/java/xyz/kmahyyg/eshopdemo/controller/OrderController.java
+++ src/main/java/xyz/kmahyyg/eshopdemo/controller/OrderController.java
@@ -3,6 +3,7 @@ package xyz.kmahyyg.eshopdemo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.kmahyyg.eshopdemo.dao.SysOrdersDao;
 import xyz.kmahyyg.eshopdemo.dao.SysUsersDao;
@@ -21,15 +22,17 @@ public class OrderController {
     private SysUsersDao sysUsersDao;
 
     @RequestMapping("/show/user/order")
-    public String showOrderOfCurrentUser() {
+    public String showOrderOfCurrentUser(Model model) {
         UserInfo currentUser = (UserInfo)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (currentUser != null) {
             String currentUsername = currentUser.getUsername();
             if (!currentUsername.isEmpty()){
                 String currentUserUid = sysUsersDao.selectByUserName(currentUsername).getUid();
                 List<SysOrders> allOrdersByUser = sysOrdersDao.selectByUserId(currentUserUid);
+                model.addAttribute("orders",allOrdersByUser);
             }
-            //TODO: render the orders data in template html
+        }else{
+            return  "login";
         }
         return "userorders";
     }
