b52228ef2ac74d2f7754df22a41dfd96ab517698
Papillon-debug <1146576451@qq.com>
2021-02-16 22:55:03 +0800

    fixed updateOrder.js bug

--- src/main/resources/static/static/js/updateOrder.js
+++ src/main/resources/static/static/js/updateOrder.js
@@ -1,13 +1,12 @@
 //计算商品总价
 function updateFinalPrice() {
-    var $tr = $("#itemTable").find("tr[id]");
+    var tr = $("#itemTable").find("tr[id]");
     var summer = 0;
-    $tr.each(function(i, dom) {
+    tr.each(function(i, dom) {
         if($(dom).find("#sItemChecked").is(":checked")){
             var num = $(dom).find(".ItemNo").val(); //商品数量
-            var name = $(dom).find("#ItemName").text();//商品名称
             if(num < 1){
-                alert("请修改商品:"+ name+" 数量！");
+                alert("请修改商品数量！");
             }else{
                 var price = num * $(dom).find(".sPrice").text(); //商品小计
                 summer += price; //总价
@@ -22,15 +21,15 @@ function submitOrder() {
 }
 //更新购物车
 function updateCart() {
-    var $tr = $("#itemTable").find("tr[id]");
-    $tr.each(function(i, dom) {
+    var tr = $("#itemTable").find("tr[id]");
+    tr.each(function(i, dom) {
         if($(dom).find("#sItemChecked").is(":checked")){
-            var ID = $(dom).find("#sItemID").val(); //商品ID
-            var NUM = $(dom).find("#sItemNo").val(); //商品数量
-            if(NUM < 1){
+            var id = $(dom).find("#sItemID").val(); //商品ID
+            var num = $(dom).find("#sItemNo").val(); //商品数量
+            if(num < 1){
                 alert("请修改商品数量！");
             }else {
-                var formData = {"id":ID,"num":NUM};
+                var formData = {"id":id,"num":num};
                 $.ajax({
                     type: "post",
                     url: "/api/cart/update",
@@ -39,6 +38,8 @@ function updateCart() {
                     encode: true,
                 }).done(function(data){
                     alert(data.message);
+                }).fail(function(){
+                    alert("更新购物车失败!");
                 })
             }
         }
@@ -46,23 +47,24 @@ function updateCart() {
 }
 //删除当前商品
 function deleteItemFromCart() {
-    var $tr = $("#itemTable").find("tr[id]");
-    $tr.each(function(i, dom) {
+    var tr = $("#itemTable").find("tr[id]");
+    tr.each(function(i, dom) {
         if($(dom).find("#sItemChecked").is(":checked")){
-            var ID = $(dom).find("#sItemID").val(); //商品ID
-            var URL = "/api/cart/delete?sItemID=" + ID;
+            var id = $(dom).find("#sItemID").val(); //商品ID
+            var url = "/api/cart/delete?sItemID=" + id;
             $.ajax({
                 type: "get",
-                url: URL,
+                url: url,
                 data: "",
                 encode: true,
             }).done(function(data){
                 alert(data.message);
+            }).fail(function(){
+                alert("删除商品失败!");
             })
 
         }
     });
-    location.reload();
 }
 
 
