fb00c649ce4970f6eaf6e1b92427f74e1b46bbee
Papillon-debug <1146576451@qq.com>
2021-02-16 15:27:48 +0800

    edit updateOrder.js

--- src/main/resources/static/static/js/updateOrder.js
+++ src/main/resources/static/static/js/updateOrder.js
@@ -1,4 +1,25 @@
-<script type="text/javascript">
+//计算商品总价
+function updateFinalPrice() {
+    var $tr = $("#itemTable").find("tr[id]");
+    var summer = 0;
+    $tr.each(function (i, dom) {
+        var num = $(dom).children(".ItemNo").val(); //商品数量
+        var price = num * $(dom).children(".sPrice").text(); //商品小计
+        summer += price;
+    });
+    $("#finalPrice").text(summer);
+}
+//提交订单
+function submitOrder() {
+
+}
+//更新购物车
+function uploadCart() {
+
+}
+//删除当前商品
+function deleteItemFromCart() {
+
+}
 
 
-</script>
