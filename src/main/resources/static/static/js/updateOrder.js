d218e7d14c9d822f75be14ae9e84aa518aa96184
Papillon-debug <1146576451@qq.com>
2021-02-16 21:52:03 +0800

    finish updatecart func

--- src/main/resources/static/static/js/updateOrder.js
+++ src/main/resources/static/static/js/updateOrder.js
@@ -5,10 +5,15 @@ function updateFinalPrice() {
     $tr.each(function(i, dom) {
         if($(dom).find("#sItemChecked").is(":checked")){
             var num = $(dom).find(".ItemNo").val(); //商品数量
-            var price = num * $(dom).find(".sPrice").text(); //商品小计
-            summer += price; //总价
+            var name = $(dom).find("#ItemName").text();//商品名称
+            if(num < 1){
+                alert("请修改商品:"+ name+" 数量！");
+            }else{
+                var price = num * $(dom).find(".sPrice").text(); //商品小计
+                summer += price; //总价
+            }
         }
-         });
+    });
     $("#finalPrice").text(summer);
 }
 //提交订单
@@ -16,8 +21,28 @@ function submitOrder() {
 
 }
 //更新购物车
-function uploadCart() {
-
+function updateCart() {
+    var $tr = $("#itemTable").find("tr[id]");
+    $tr.each(function(i, dom) {
+        if($(dom).find("#sItemChecked").is(":checked")){
+            var ID = $(dom).find("#sItemID").val(); //商品ID
+            var NUM = $(dom).find("#sItemNo").val(); //商品数量
+            if(NUM < 1){
+                alert("请修改商品数量！");
+            }else {
+                var formData = {"id":ID,"num":NUM};
+                $.ajax({
+                    type: "post",
+                    url: "/api/cart/update",
+                    data: formData,
+                    dataType: "json",
+                    encode: true,
+                }).done(function(data){
+                    alert(data.message);
+                })
+            }
+        }
+    });
 }
 //删除当前商品
 function deleteItemFromCart() {
@@ -37,7 +62,7 @@ function deleteItemFromCart() {
 
         }
     });
-
+    location.reload();
 }
 
 
