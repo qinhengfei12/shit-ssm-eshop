9d3c215ff7598fa272770a7edb07955da792ef89
Papillon-debug <1146576451@qq.com>
2021-02-17 18:06:36 +0800

    finished submitoreder func

--- src/main/resources/static/static/js/updateOrder.js
+++ src/main/resources/static/static/js/updateOrder.js
@@ -17,7 +17,32 @@ function updateFinalPrice() {
 }
 //提交订单
 function submitOrder() {
-
+    var saveDataArray = [];
+    var cartTableElem = $("#itemTable").find("tr[id]");
+    cartTableElem.each(function(cIndex, cElement) {
+        if($(cElement).find("#sItemChecked").is(":checked")){
+            var cElemId= $(cElement).find("#sItemID").val(); //商品ID
+            var cElemNum = $(cElement).find("#sItemNo").val(); //商品数量
+            if(cElemNum < 1){
+                alert("请修改商品数量!");
+            }else{
+                var itemData = {"itemId":cElemId,"itemNo":cElemNum};
+                saveDataArray.push(itemData);
+            }
+        }
+    });
+    var postItemData = {"items":saveDataArray};
+    $.ajax({
+        type: "post",
+        url: "/api/user/order",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(postItemData),
+    }).done(function (respData) {
+        alert(respData.message);
+    }).fail(function () {
+        alert("提交订单失败!");
+    })
 }
 //更新购物车
 function updateCart() {
@@ -36,8 +61,8 @@ function updateCart() {
                     data: formData,
                     dataType: "json",
                     encode: true,
-                }).done(function(respdata){
-                    alert(respdata.message);
+                }).done(function(respData){
+                    alert(respData.message);
                 }).fail(function(){
                     alert("更新购物车失败!");
                 })
@@ -57,8 +82,8 @@ function deleteItemFromCart() {
                 url: cItemUrl,
                 data: "",
                 encode: true,
-            }).done(function(respdata){
-                alert(respdata.message);
+            }).done(function(respData){
+                alert(respData.message);
             }).fail(function(){
                 alert("删除商品失败!");
             })
