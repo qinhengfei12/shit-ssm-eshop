8020d65a753712b853c439d21678c74ad28b208c
qinhengfei12 <2678583019@qq.com>
2021-02-19 17:31:21 +0800

    test4

--- src/main/java/xyz/kmahyyg/eshopdemo/controller/userInfoController.java
+++ src/main/java/xyz/kmahyyg/eshopdemo/controller/userInfoController.java
@@ -53,7 +53,7 @@ public class userInfoController {
             System.out.println(userSysUsersDao.toString());
 
         if (sysUsersDao.updateByUserIdSelective(userSysUsersDao) == 1){
-            System.out.println("ppppppppppp");
+            System.out.println("77777");
         }
 
 
